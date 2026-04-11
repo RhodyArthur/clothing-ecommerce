94dad7dd513f27cb8452365d13d5e96a51285986
RhodyArthur <rhodaarthur48@gmail.com>
2026-04-11 19:04:17 +0000

    feat: add authentication routes for login, register, forgot-password, and reset-password

---
+++ src/app/features/auth/auth.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+
+export const AUTH_ROUTES: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./login/login').then(m => m.Login)
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./register/register').then(m => m.Register)
+  },
+  {
+    path: 'forgot-password',
+    loadComponent: () =>
+      import('./forgot-password/forgot-password')
+        .then(m => m.ForgotPassword)
+  },
+  {
+    path: 'reset-password',
+    loadComponent: () =>
+      import('./reset-password/reset-password')
+        .then(m => m.ResetPassword)
+  }
+];
