561abe5fa9e75ff324b006557cfd63ffc2b0b5c9
RhodyArthur <rhodaarthur48@gmail.com>
2026-04-11 20:22:16 +0000

    feat: add environment configuration for development and production

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  supabase: {
+    url: 'https://sdtwueuwrkthhbnwjhvv.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkdHd1ZXV3cmt0aGhibndqaHZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyNDA0MDMsImV4cCI6MjA5MDgxNjQwM30.gIqAyozHHvq62Njs5xXcERNxe-z_1Rz7VaD3D1qV2Mo'
+  }
+};
